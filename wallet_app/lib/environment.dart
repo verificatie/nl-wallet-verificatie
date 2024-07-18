2e0ed7b34f49fec774258e9527b89dbe3cde8fab
Erickson Hyppolite Poel <20659380+EricksonAtHome@users.noreply.github.com>
2024-07-18 02:31:26 +0200

    Add files via upload

---
+++ wallet_app/lib/environment.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+
+class Environment {
+  Environment._();
+
+  static bool get mockRepositories => const bool.fromEnvironment('MOCK_REPOSITORIES', defaultValue: true);
+
+  static bool get isTest => Platform.environment.containsKey('FLUTTER_TEST');
+
+  static String get sentryDsn => const String.fromEnvironment('SENTRY_DSN');
+
+  static bool get hasSentryDsn => sentryDsn.isNotEmpty;
+
+  static String get sentryEnvironment =>
+      const String.fromEnvironment('SENTRY_ENVIRONMENT', defaultValue: 'unspecified');
+
+  static String? sentryRelease() {
+    const release = String.fromEnvironment('SENTRY_RELEASE', defaultValue: 'unspecified');
+    if (release == 'unspecified') {
+      return null;
+    } else {
+      return release;
+    }
+  }
+}

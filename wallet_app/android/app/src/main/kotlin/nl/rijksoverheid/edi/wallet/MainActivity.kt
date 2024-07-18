4824e0cd9f53ba777048b48be71f25a7c9703af1
Erickson Hyppolite Poel <20659380+EricksonAtHome@users.noreply.github.com>
2024-07-18 02:21:02 +0200

    Add files via upload

---
+++ wallet_app/android/app/src/main/kotlin/nl/rijksoverheid/edi/wallet/MainActivity.kt
@@ -0,0 +1,31 @@
+package nl.rijksoverheid.edi.wallet
+
+import android.os.Bundle
+import android.view.WindowManager
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        window.clearFlags(WindowManager.LayoutParams.FLAG_SECURE);
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            window.clearFlags(WindowManager.LayoutParams.FLAG_SECURE)
+        } else {
+            window.addFlags(WindowManager.LayoutParams.FLAG_SECURE)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        window.addFlags(WindowManager.LayoutParams.FLAG_SECURE);
+    }
+}
